7a351a303870c485dcbdba2ed15b5df535a1078e
klauskneupner <klaus@kneupner.de>
2019-04-12 06:52:46 +0200

    Initial Commit

---
+++ SwiftGraphviz/SwiftGraphviz.swift
@@ -0,0 +1,11 @@
+//
+//  SwiftGraphviz.swift
+//  SwiftGraphviz
+//
+//  Created by Klaus Kneupner on 12/04/2019.
+//  Copyright © 2019 Klaus Kneupner. All rights reserved.
+//
+
+class SwiftGraphviz {
+
+}
