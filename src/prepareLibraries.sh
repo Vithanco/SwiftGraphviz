fa80bd991c79844d704f67d754ebbf84221cad21
klauskneupner <klaus@kneupner.de>
2019-04-20 06:24:59 +0200

    recompiled libraries for 10.13

--- src/prepareLibraries.sh
+++ src/prepareLibraries.sh
@@ -1,11 +1,17 @@
 INSTALLER_ROOT="/Users/klauskneupner"
-GVROOT="${INSTALLER_ROOT}/graphviz"  
-GVTARGET="${INSTALLER_ROOT}/Documents/xcode/SwiftGraphviz/gvDump"
+GVROOT="${INSTALLER_ROOT}/graphviz"
+#GVTARGET="${INSTALLER_ROOT}/Documents/xcode/SwiftGraphviz/gvDump"
 PLATFORM_DEVELOPER_BIN_DIR="/usr/bin"
+rm -f config.cache
+
+export CFLAGS="-mmacosx-version-min=10.13"
+export CPPFLAGS="-mmacosx-version-min=10.13"
+export CXXFLAGS="-mmacosx-version-min=10.13"
+
 
 ./configure \
   --disable-dependency-tracking \
-  --enable-shared=no \
+  --enable-shared=yes \
   --enable-static=yes \
   --enable-ltdl=no \
   --enable-swig=no \
@@ -22,16 +28,16 @@ PLATFORM_DEVELOPER_BIN_DIR="/usr/bin"
  --with-quartz=yes \
   --with-visio=yes \
    --with-x=no \
-   CC="${PLATFORM_DEVELOPER_BIN_DIR}/clang" \
-   CPP="${PLATFORM_DEVELOPER_BIN_DIR}/clang -E" \
-   CXX="${PLATFORM_DEVELOPER_BIN_DIR}/clang++" \
-   OBJC="${PLATFORM_DEVELOPER_BIN_DIR}/clang" \
-   LD="${PLATFORM_DEVELOPER_BIN_DIR}/ld" 
+   CC="${PLATFORM_DEVELOPER_BIN_DIR}/clang -mmacosx-version-min=10.13" \
+   CPP="${PLATFORM_DEVELOPER_BIN_DIR}/clang -E -mmacosx-version-min=10.13" \
+   CXX="${PLATFORM_DEVELOPER_BIN_DIR}/clang++ -mmacosx-version-min=10.13" \
+   OBJC="${PLATFORM_DEVELOPER_BIN_DIR}/clang -mmacosx-version-min=10.13" \
+   LD="${PLATFORM_DEVELOPER_BIN_DIR}/ld"
 
 make
 
-rm -rf ${GVTARGET}
-mkdir -p ${GVTARGET}
-find . -type f -name '*_C.a' -exec cp -i {} ${GVTARGET} \;
-find . -type f -name '*.h' -exec cp {} -i ${GVTARGET} \;
+#rm -rf ${GVTARGET}
+#mkdir -p ${GVTARGET}
+#find . -type f -name '*_C.a' -exec cp -i {} ${GVTARGET} \;
+#find . -type f -name '*.h' -exec cp {} -i ${GVTARGET} \;
                                                                                                           
