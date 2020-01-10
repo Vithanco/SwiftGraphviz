4c58485ecc6545fdf5e1ed0bd2a673159ac50567
klauskneupner <klaus@kneupner.de>
2020-01-10 07:16:52 +0100

    not sure I need security bookmarks Decision Tree works again

--- Sources/SwiftGraphviz/GVGraph.swift
+++ Sources/SwiftGraphviz/GVGraph.swift
@@ -20,5 +20,36 @@ public extension UnsafeMutablePointer where Pointee == Agraph_t {
         fsync(fileno(f))
         fclose(f)
     }
+    
+    /// adapted from: https://stackoverflow.com/questions/59653517/how-to-use-file-descriptor-to-divert-write-to-file-in-swift/59654364#59654364
+    var asString: String? {
+        let pipe = Pipe()
+        do {
+            try use(fileDescriptor: pipe.fileHandleForWriting.fileDescriptor, mode: "w") { filePointer in
+                agwrite(self, filePointer)
+            }
+            let data = pipe.fileHandleForReading.readDataToEndOfFile()
+            guard let output = String(data: data, encoding: .utf8) else {
+                return nil
+            }
+            return output
+        } catch {
+            return nil
+        }
+    }
+}
 
+@discardableResult fileprivate func use<R>(
+    fileDescriptor: Int32,
+    mode: UnsafePointer<Int8>!,
+    closure: (UnsafeMutablePointer<FILE>) throws -> R
+) throws -> R {
+    // Should prob remove this `!`, but IDK what a sensible recovery mechanism would be.
+    guard let filePointer = fdopen(fileDescriptor, mode) else {
+        throw CannotOpenFileDescriptor()
+    }
+    defer {
+        fclose(filePointer)
+    }
+    return try closure(filePointer)
 }
